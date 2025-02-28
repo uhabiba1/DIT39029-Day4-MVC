0eea0d0c68318d605e4bbb0b16734b8c3283b6a0
uhabiba1 <u.habiba@uwinnipeg.ca>
2025-02-27 21:48:12 -0600

    Update CategoryGrids.js

--- components/CategoryGrids.js
+++ components/CategoryGrids.js
@@ -1,4 +1,4 @@
-import { Pressable, View, Text, StyleSheet, Platform } from 'react-native';
+import { Pressable, View, Text, StyleSheet } from 'react-native';
 
 const CategoryGrids = ({ title, color }) => {
   return (
